da930e23d0b76a7324084c06abd54dd5147d2344
KaarenTenson <tensonkaaren@gmail.com>
2024-07-09 10:48:03 +0300

    algus

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,11 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.util.Random;
 public class Main {
     public static void request(String url1, ArrayList<String> urlid, ArrayList<String> nimed, FileWriter writer){
         try {
+            System.out.println(url1);
             URL url = new URL(url1);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
@@ -56,33 +58,60 @@ public class Main {
             }
 
         } catch (Exception e) {
-            System.out.println("failed");
+            System.out.println(e.toString());
         }
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         ArrayList<String> nimed = new ArrayList<String>();  /////////llist kaartidde nimede jaoks
         ArrayList<String> urlid = new ArrayList<String>();  //////////list kaartide urlide jaoks
-
+        int LandsCount=0;
+        int basicp=30;
+        String colors;
+        String nonlandsearch;
+        String landsearch;
+        char[] varvid={'W','U','B','G','R'};
         Scanner s = new Scanner(System.in);
         System.out.println("deck size:");
         int decksize = Integer.parseInt(s.nextLine());
 
-        System.out.println("how many lands?");
-        int LandsCount = Integer.parseInt(s.nextLine());
+        System.out.println("random?(T/F)");
+        String kaslihtne=s.nextLine();
+        if(!kaslihtne.equals("T")) {
 
-        System.out.println("% of nonbasiclands (only int)");
-        int basicp = Integer.parseInt(s.nextLine());
+            System.out.println("how many lands?");
+            LandsCount = Integer.parseInt(s.nextLine());
 
-        System.out.println("colors (WUBRG)");
-        String colors = s.nextLine();
+            System.out.println("% of nonbasiclands (only int)");
+            basicp = Integer.parseInt(s.nextLine());
 
-        System.out.println("nonland card search (copy everything after q=)");
-        String nonlandsearch = s.nextLine();
+            System.out.println("colors (WUBRG)");
+            colors = s.nextLine();
 
-        System.out.println("land card search (copy everything after q=)");
-        String landsearch = s.nextLine();
-        
+            System.out.println("nonland card search (copy everything after q=)");
+            nonlandsearch = s.nextLine();
+
+            System.out.println("land card search (copy everything after q=)");
+            landsearch = s.nextLine();
+        }
+        else{
+            LandsCount=(int)(decksize*0.4);
+            Random rand=new Random();
+            StringBuilder build=new StringBuilder();
+            while (build.length()<2){
+                char varv=varvid[rand.nextInt(varvid.length)];
+                if(build.isEmpty() ||build.charAt(0)!=varv){
+                    build.append(varv);
+                }
+            }
+            colors=build.toString();
+            System.out.println(colors);
+            nonlandsearch="-type%3Aland+commander%3A"+colors+"+%28game%3Apaper%29";
+            //nonlandsearch="-type%3Aland+color<%3DWU+%28game%3Apaper%29";
+            System.out.println(nonlandsearch);
+            landsearch="-type%3Aland+commander%3A"+colors+"+%28game%3Apaper%29";
+
+        }
         System.out.println(LandsCount);
         int nonlands = decksize-LandsCount;
         System.out.println(nonlands);
@@ -96,7 +125,7 @@ public class Main {
 
         for(int i = 0; i<nonlands ; i++) {  //////////////lisab landid
             Thread.sleep(50);
-            request("https://api.scryfall.com/cards/random?q="+nonlandsearch,urlid, nimed,writer);
+            request((String) ("https://api.scryfall.com/cards/random?q="+nonlandsearch),urlid, nimed,writer);
         }
         for(int i = 0; i<nonbasiclands ; i++) {  /////////////lisab nonbasic landid
             Thread.sleep(50);
@@ -114,6 +143,7 @@ public class Main {
                 landid.put(de,hulk);
             }
         }
+        System.out.println("aa");
             if(landid.containsKey('W')){
                 Thread.sleep(50);
                 writer.write("Plains"+"\n");
